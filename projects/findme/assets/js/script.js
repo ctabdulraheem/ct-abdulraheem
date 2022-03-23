ad813bb8441179d2650edcc39dff9aedad6f67db
mohammed abdul raheem <mohammed.abdulraheem@coalitiontechnologies.com>
2022-03-23 07:15:34 +0530

    adding new api for findme

--- projects/findme/assets/js/script.js
+++ projects/findme/assets/js/script.js
@@ -3,62 +3,33 @@
     $.fn.findMe = function () {
 
         let fineMeElement = $(this);
-        function findMeData(codeFm, countryFm, regionFm, region, cityFm, zipFm, timeZoneFm) {
+        function findMeData(codeFm, countryFm, regionFm, region, cityFm, stateFm, ipFm) {
             let countryImg = `<div class="country-img"><img src="https://countryflagsapi.com/png/${codeFm}" alt="" loading="lazy"/> </div>`;
 
             let countryName = `<div class="country-name">
             <h1>${countryFm} <span id="country-code">${codeFm}</span></h1></div>`;
-            let regionName = `<div class="region-name"><p>${regionFm} <span id="region">${region}</span></p></div>`;
+            let regionName = `<div class="continent-name"><p>${regionFm} <span id="region">${region}</span></p></div>`;
             let countryCity = `<div class="city"><h2>${cityFm}</h2></div>`;
-            let countryZip = `<div class="zip"><p>${zipFm}</p></div>`;
-            let countryTimeZone = `<div class="timezone"><p>${timeZoneFm}</p></div>`;
+            let countrystate = `<div class="state"><p>${stateFm}</p></div>`;
+            let countryip = `<div class="ip"><p>${ipFm}</p></div>`;
 
-            let fineMe = `${countryImg} ${countryName} ${regionName} ${countryCity} ${countryZip} ${countryTimeZone}`;
+            let fineMe = `${countryImg} ${countryName} ${regionName} ${countryCity} ${countrystate} ${countryip}`;
             fineMeElement.html(fineMe);
         }
 
-        let methodType = "GET";
-        let url = `http://ip-api.com/json`;
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
+        $.ajax({
+            dataType: "json",
+            url: "https://api.db-ip.com/v2/free/self",
+            success: function (findmeResult) {
 
-            if (this.readyState == 4 && this.status == 200) {
-                let findmeResult = JSON.parse(this.responseText);
-                if (findmeResult.status == 'success') {
-                    findMeData(findmeResult.countryCode, findmeResult.country, findmeResult.regionName, findmeResult.region, findmeResult.city, findmeResult.zip, findmeResult.timezone);
-                    
+                if (findmeResult.countryCode !== undefined && findmeResult.countryName !== undefined && findmeResult.continentCode !== undefined && findmeResult.city !== undefined && findmeResult.stateProv !== undefined && findmeResult.ipAddress !== undefined) {
+                    findMeData(findmeResult.countryCode, findmeResult.countryName, findmeResult.continentName, findmeResult.continentCode, findmeResult.city, findmeResult.stateProv, findmeResult.ipAddress);
                 } else {
                     let errorMsg = '<p class="errormsg">Some thing went wrong</p>'
                     fineMeElement.html(errorMsg);
                 }
             }
-           
-        }
-        
-        xhr.open(methodType,url,true);
-        xhr.send();
-
-
-
+        });
     }
 
 }($));
-
-
-
-
-var endpoint = 'http://ip-api.com/json/?fields=status,message,countryCode';
-
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		let response = JSON.parse(this.responseText);
-        console.log(response)
-		if(response.status == 'success') {
-            console.log(response.countryCode)
-		}
-		
-	}
-};
-xhr.open('GET', endpoint, true);
-xhr.send();
